49e5427945c840ea58279e9fb9dff5dae8bdcc54
unknown <kimbling303@gmail.com>
2021-04-28 00:15:44 +0900

    use variety view

---
+++ app/src/main/java/com/example/midterm12_20172127/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.midterm12_20172127;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);*/
+    }
+
+}
